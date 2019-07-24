962dad78c6e4e97731a1161b417007db00083d94
Providence-Design <baabaprovidence@gmail.com>
2019-07-24 07:36:21 -0700

    data api

--- traffic.py
+++ traffic.py
@@ -1,17 +1,39 @@
-from flask import *
+from flask import Flask, render_template, request
+import sqlite3 as lite
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('tryhow_css_login_form_modal.html')
+    return render_template('home.html')
 
 @app.route('/data')
 def data():
-    return render_template('data.html')
+    pass
+
+@app.route('/addrec', methods=['POST'])
+def add_record():
+    try:
+        image_url = request.form['image_url']
+        timestamp = request.form['timestamp']
+        speed = request.form['speed']
+        camera = request.form['camera']
+
+        with lite.connect('traffic.db') as con:
+            cur = con.cursor()
+            cur.execute('''INSERT INTO data(image, time_stamp, speed, camera_id) VALUES(?, ?, ?, ?)''', (image_url, timestamp, speed, camera))
+
+            con.commit()
+            msg = 'Record successfully added!'
+    except:
+        con.rollback()
+        msg = 'error occured'
+    finally:
+        con.close()
+        return msg
 
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
-    app.run(debug=True, host='0.0.0.0',port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
